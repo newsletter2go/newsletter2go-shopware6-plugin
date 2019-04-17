decc0203b1c7a63228e799a27c6ec5f8a89b7c75
ahmad <ahmad@newsletter2go.com>
2019-04-17 14:20:48 +0200

    adjust groups endpoint

--- src/Controller/Api/GroupController.php
+++ src/Controller/Api/GroupController.php
@@ -14,34 +14,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class GroupController extends AbstractController
 {
     /**
-     * @Route("/api/{version}/n2g/getGroups", name="api.action.n2g.getGroups", methods={"GET"})
+     * @Route("/api/{version}/n2g/groups", name="api.action.n2g.getGroups", methods={"GET"})
      * @return JsonResponse
      */
     public function getGroupsAction(): JsonResponse
     {
-        $groups = $this->getCustomerGroups();
-        if ($groups instanceof EntityCollection) {
+        $response = [];
+        try {
+
+            $groups = $this->getCustomerGroups();
             $response['success'] = false;
-            $response['data'] = $groups->toArray();
-        } else {
+            $response['data'] = $groups->getElements();
+
+        } catch (\Exception $exception) {
             $response['success'] = false;
-            $response['error'] = $groups;
+            $response['error'] = $exception->getMessage();
         }
 
         return new JsonResponse($response);
     }
 
-    private function getCustomerGroups()
+    private function getCustomerGroups(): EntityCollection
     {
-        try {
-            /** @var EntityRepositoryInterface $groupRepository */
-            $groupRepository = $this->container->get('customer_group.repository');
-            /** @var EntityCollection $groups */
-            $groups = $groupRepository->search(new Criteria(), Context::createDefaultContext())->getEntities();
-            return $groups;
+        /** @var EntityRepositoryInterface $groupRepository */
+        $groupRepository = $this->container->get('customer_group.repository');
+        $result = $groupRepository->search(new Criteria(), Context::createDefaultContext());
 
-        } catch (\Exception $exception) {
-            return $exception->getMessage();
-        }
+        return $result->getEntities();
     }
 }
